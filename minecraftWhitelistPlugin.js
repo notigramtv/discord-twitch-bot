a96230dd93d8e33b7c2af3e0bee44fcf947dea8c
Notigram <tv.notigram@gmail.com>
2025-12-26 16:42:38 +0100

    Aggiornamento bot generale

--- minecraftWhitelistPlugin.js
+++ minecraftWhitelistPlugin.js
@@ -5,27 +5,22 @@ const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 // CONFIG
-const { client } = require('./bot');
-
 const WHITELIST_CHANNEL_ID = '1450532753115320501';
 const OUTPUT_CHANNEL_ID = '1454127695192653845';
 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
-  if (message.channel.id !== WHITELIST_CHANNEL_ID) return;
+const { client } = require('./bot');
 
-  console.log('📩 Messaggio intercettato nel canale whitelist');
-  console.log(message.content);
+client.once('ready', () => {
+  console.log('✅ Minecraft Whitelist Plugin attivo');
 
-  const outputChannel = await client.channels.fetch(OUTPUT_CHANNEL_ID);
-  if (!outputChannel) {
-    console.log('❌ Canale output non trovato');
-    return;
-  }
+  client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.channel.id !== '1450532753115320501') return;
 
-  await outputChannel.send(
-    `🧪 TEST OK\nMessaggio ricevuto:\n\n${message.content}`
-  );
+    console.log('📩 Messaggio intercettato nel canale whitelist');
 
-  console.log('📤 Messaggio di test inviato');
+    const outputChannel = await client.channels.fetch('1454127695192653845');
+    await outputChannel.send('🧪 TEST: messaggio ricevuto');
+  });
 });
+
